45f4f87a337804008e57f1a271d7f7f9e23eb0ce
sahilpayla <1shraddhanand@gmail.com>
2022-06-21 21:56:14 +0530

    sahil

---
+++ src/model/blogModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const blog = new mongoose.Schema({
+
+    title:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    authorId : {
+        type: ObjectId,
+        required: true,
+        ref : "authorProject"
+    },
+    tags: {
+        type: [String]
+    },
+    category:{
+        type: [String],
+        required: true
+    },
+    subcategory:{
+        type: [String],
+        required: true
+    },
+    isDeleted: {
+        type: Boolean, 
+        default: false
+    },
+    deletedAt: {
+        type: String, 
+        default: " "
+    },
+    isPublished: {
+        type: Boolean ,
+        default: false
+    },
+     publishedAt: {
+        type: String,
+        default: " "
+    }
+
+}, { timestamps: true } );
+
+module.exports = mongoose.model('blogProject', blog) //BlogProjects
