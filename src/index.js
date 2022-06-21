e3c738f7558694fdc7cb2e7aef5483f9dd1e588d
sahilpayla <1shraddhanand@gmail.com>
2022-06-21 20:27:10 +0530

    shraddhanand

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://sahilpayla:T6cALrdO1y8L5HtJ@sahilpaylacluster.h8dndkb.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authors = require("../controller/authorController")
+
+router.get("/me", function (req, res) {
+    res.send("My first ever api!")
+}) 
+
+router.post("/createAuthor", authors.createAuthor)
+router.post("/createBlog", authors.createBlog)
+
+
+module.exports = router;
