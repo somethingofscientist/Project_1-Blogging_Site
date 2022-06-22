501fc4e9a6f1d8bbc917166cb152b32f399d9dc9
arpitmaheshwar <apmaheshwari47@gmail.com>
2022-06-22 09:30:59 +0530

    output

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,8 +1,7 @@
-const { ObjectId, ObjectID } = require("bson")
+
 const mongoose = require("mongoose")
-// const jwt = require("jsonwebtoken")
-const authorModel = require("../models/authorModel")
-const blogModel = require("../models/blogModel")
+const authorModel = require("../model/authorModel")
+const blogModel = require("../model/blogModel")
 
 
 const createAuthor = async function (req, res) {
@@ -13,10 +12,16 @@ const createAuthor = async function (req, res) {
 
 const createBlog = async function (req, res) {
     let data = req.body
-    if(!data.authorId) {return res.send("author Id Is Not Valid")}
+    if(!data.authorId) {return res.status(400).send("author Id Is Not Valid")}
     const savedData = await blogModel.create(data)
+    res.status(201).send({ data: savedData }) //ALL GOOD... //status(201)- OK
+}
+const getBlog = async function (req, res) {
+    const savedData = await blogModel.find().$and[{isDeleted:false},{ispublished:true}]
+    if(!savedData){return res.send("not found")}
     res.status(201).send({ data: savedData }) //ALL GOOD... //status(201)- OK
 }
 
 module.exports.createAuthor = createAuthor
 module.exports.createBlog = createBlog
+module.exports.getBlog = getBlog
