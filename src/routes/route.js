9244fd111f43a41fb0bc89b63fb89e98b4a071a0
sahilpayla <1shraddhanand@gmail.com>
2022-06-23 12:45:57 +0530

    login

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,11 +8,12 @@ router.get("/me", function (req, res) {
 
 router.post("/createAuthor", authors.createAuthor)
 router.post("/createBlogs", authors.createBlog)
-router.post("/login",authors.loginAuthor)
-
 router.get("/getBlog", authors.getBlog)
 router.put("/updateBlog/:blogId", authors.updateBlog)
 router.delete("/deleteBlog/:blogId",authors.deleteBlog)
 router.delete("/deleteBlogByParams", authors.deleteBlogByParams)
+
+//NEW
+router.post("/login",authors.loginAuthor)
 
 module.exports = router;
