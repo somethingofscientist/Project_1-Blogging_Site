0b031fc9bdff43b1dd2369bd79e7cf8accf57fd3
sahilpayla <1shraddhanand@gmail.com>
2022-06-26 19:07:20 +0530

    user sahil

--- src/github.js
+++ src/github.js
@@ -1,30 +1,52 @@
 
-const updateBlog = async function (req, res) {
+const getBlog = async function (req, res) {
     try {
-
-        let data = req.body
-        let blogId = req.params.blogId;
-        if (Object.keys(data).length == 0) {
-            return res.status(400).send({ msg: "Please provide blog details" })
+  
+      let data = req.query;
+      let filter = {
+  
+        //DELETE BLOG POINT NUMBER 1
+        // isDeleted: false,
+        // isPublished: true,
+        ...data
+      }
+      const { authorId, category, tags, subcategory } = data
+      if (category) {
+        let verifyCategory = await blogModel.find({ category: category })
+        if (!verifyCategory) {
+          return res.status(400).send({ status: false, msg: "No Blogs In This Category Exist" })
         }
-
-        let title = req.body.title
-        let body = req.body.body
-        let tags = req.body.tags
-        let subcategory = req.body.subcategory
-        let Data = Date.now()
-
-        const updateblog = await blogModel.findByIdAndUpdate
-            ({ _id: blogId, isDeleted: false },
-                { $set: { title: title, body: body, tags: tags, subcategory: subcategory, isPublished: true, publishedAt: Data } },
-                { new: true })
-
-        console.log(updateblog)
-        // blogid exist ka bar m TA se dicuss
-        // if do not provide the blog id
-        res.status(200).send({ status: true, data: updateblog })
-
-    } catch (err) {
-        res.status(500).send({ msg: err.message })
+      }
+      if (tags) {
+        let verifyTag = await blogModel.find({ tags: tags })
+        if (!verifyTag) {
+          return res.status(400).send({ status: false, msg: "No Blogs In This Tags Exist" })
+        }
+      }
+      if (subcategory) {
+        let verifySubcategory = await blogModel.find({ subcategory: subcategory })
+        if (!verifySubcategory) {
+          return res.status(400).send({ status: false, msg: "No Blogs In This SubCategory Exist" })
+        }
+      }
+      if (authorId) {
+        if (!mongoose.isValidObjectId(authorId))
+          return res.status(400).send({ status: false, msg: "Invalid AuthorId" })
+      }
+  
+      let getRecord = await blogModel.find(filter).populate("authorId")
+  
+  
+      if (getRecord.length == 0) {
+        return res.status(404).send({
+          data: "Data Not Found"
+        })
+      }
+  
+      return res.status(200).send({ status: true,msg:"get", data: getRecord })
     }
-}
+    catch (err) {
+      res.status(500).send({ msg: err.name })
+    }
+    }
+  
